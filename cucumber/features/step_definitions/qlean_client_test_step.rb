84cd636ae4290b4d80bfde8ece06d946f2c9577b
Dnar <chanky@list.ru>
2017-02-16 21:58:15 +0300

    end of the first step

--- cucumber/features/step_definitions/qlean_client_test_step.rb
+++ cucumber/features/step_definitions/qlean_client_test_step.rb
@@ -7,11 +7,16 @@ When(/^I change room and bathroom$/) do
   f = page.first('form[data-test-id="orderCreationForm"]')
   f.find('[data-test-id="counterDecrementButton.serviceId1"]').click
   f.should have_content '1-комнатная'
-  page.save_screenshot
+  f.click_button('Рассчитать стоимость')
+  #page.save_screenshot
 end
 
 When(/^I should see in popup "([^"]*)"$/) do |arg1|
+  
+  if page.has_button? "Запросить код повторно" && page.has_field?('code')
+    page.fill_in('code', :with => sms_code)
 
+  end
 end
 
 When(/^I fill in popup field with sms_code$/) do
