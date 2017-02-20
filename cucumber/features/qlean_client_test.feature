913f8ccbb759d5777010b312afd400f6c9145a0b
Dnar <chanky@list.ru>
2017-02-20 16:24:38 +0300

    add test with sms-code

--- cucumber/features/qlean_client_test.feature
+++ cucumber/features/qlean_client_test.feature
@@ -5,8 +5,5 @@ Feature: Qlean-client
   Scenario: Login and first order
     Given I am on page "http://localhost:8080"
     When I change room and bathroom
-    When I fill in field with the text "9999999999"
-    When I click button "Рассчитать стоимость"
-    When I should see in popup "Введите код из смс"
     When I fill in popup field with sms_code
-    Then I have see in result "Оформление заказа"
+    Then I have see in result "Выберите адрес"
--- cucumber/features/step_definitions/qlean_client_test_step.rb
+++ cucumber/features/step_definitions/qlean_client_test_step.rb
@@ -1,4 +1,6 @@
 require 'bddfire'
+require 'open-uri'
+require 'nokogiri'
 
 #################__Login to Qlean__#################
 
@@ -7,18 +9,21 @@ When(/^I change room and bathroom$/) do
   f = page.first('form[data-test-id="orderCreationForm"]')
   f.find('[data-test-id="counterDecrementButton.serviceId1"]').click
   f.should have_content '1-комнатная'
-  f.click_button('Рассчитать стоимость')
-  #page.save_screenshot
-end
-
-When(/^I should see in popup "([^"]*)"$/) do |arg1|
-  
-  if page.has_button? "Запросить код повторно" && page.has_field?('code')
-    page.fill_in('code', :with => sms_code)
-
-  end
+  f.find('[data-test-id="orderCreationForm.submitButton"]').click
 end
 
 When(/^I fill in popup field with sms_code$/) do
+    url = 'http://localhost:3000/admin/sms_codes'
+    html = open(url)
+    code = Nokogiri::HTML(html).css('tbody td:nth-child(2)').text
 
-end
+    if page.has_field?('code') #&& page.has_button? "Запросить код повторно"
+      page.fil_in('code', with => code)
+    else
+      
+    end
+  end
+
+  Then(/^I have see in result$/) do |secondStep|
+    page.should have_content secondStep
+  end
