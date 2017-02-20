6f5a34af8053806a4bd110ea54f48160d846f9ef
Dnar <chanky@list.ru>
2017-02-20 21:09:05 +0300

    add some steps

--- cucumber/features/qlean_client_test.feature
+++ cucumber/features/qlean_client_test.feature
@@ -6,4 +6,4 @@ Feature: Qlean-client
     Given I am on page "http://localhost:8080"
     When I change room and bathroom
     When I fill in popup field with sms_code
-    Then I have see in result "Выберите адрес"
+    Then I should see some text
--- cucumber/features/step_definitions/qlean_client_test_step.rb
+++ cucumber/features/step_definitions/qlean_client_test_step.rb
@@ -9,21 +9,35 @@ When(/^I change room and bathroom$/) do
   f = page.first('form[data-test-id="orderCreationForm"]')
   f.find('[data-test-id="counterDecrementButton.serviceId1"]').click
   f.should have_content '1-комнатная'
+  f.fill_in('phone', :with => '9256774343')
   f.find('[data-test-id="orderCreationForm.submitButton"]').click
+  sleep 2
 end
 
 When(/^I fill in popup field with sms_code$/) do
     url = 'http://localhost:3000/admin/sms_codes'
     html = open(url)
-    code = Nokogiri::HTML(html).css('tbody td:nth-child(2)').text
+    code = Nokogiri::HTML(html).css('tbody tr:first td:nth-child(2)').text
 
-    if page.has_field?('code') #&& page.has_button? "Запросить код повторно"
-      page.fil_in('code', with => code)
+    page.save_screenshot
+
+    if page.has_field?('code')
+      page.fill_in('code', :with => code)
     else
-      
+      puts 'first order'
     end
   end
 
-  Then(/^I have see in result$/) do |secondStep|
-    page.should have_content secondStep
-  end
+Then(/^I should see some text$/) do
+    if current_url.split('/').last == 'additional'
+      puts 'success'
+    else
+      puts "Error"
+    end
+  page.should have_content 'Оформление заказа'
+  page.should have_content 'Адрес квартиры'
+  page.should have_content 'Когда к вам приехать'
+  page.should have_content 'Способ оплаты'
+  page.should have_content 'Уборка квартиры c 1 жилой и 1 ванной комнатой'
+end
+
